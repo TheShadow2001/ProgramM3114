c63be88b84b3f8d7559a88e4207c6666d146d9e5
TheShadow2001 <95100783+TheShadow2001@users.noreply.github.com>
2021-11-26 21:44:30 +0300

    Changed the code
    I disposed again code and add some tiltle

--- lap5.cpp
+++ lap5.cpp
@@ -3,6 +3,7 @@
 int main(){
 	int a[9] = {88,112, 6467, 325, 878, 3, 77, 8, 99};
 	// fill b and c as a
+	//Task 1
 	int b[2][2];
 	b[0][0] = 88;
 	b[0][1] = 112;
@@ -13,21 +14,15 @@ int main(){
 	c[0][1] = 3;
 	c[1][0] = 77;
 	c[1][1] = 8;
-	
-	
-	
-	
+	//Task 2
 	int d[100][100];
-	 	printf("Matrix d :\n");
-	 	for ( int i = 0; i <= 1; i++ ){
-	 		for ( int j = 0;j <= 1; j++){
-			 
+	 printf("Matrix d :\n");
+	 for ( int i = 0; i <= 1; i++ ){
+	 	for ( int j = 0;j <= 1; j++){
 		printf("\t%d ", d[i][j]=b[i][j] * c[i][j]);
 		}
 		printf("\n");
 	}
-	
-	
 	return 0;
 }
 
